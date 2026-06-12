8603fe2e999b0dd616c50131bae81175f40e0e8e
beegormif <a.stesmith1@gmail.com>
2026-06-12 13:38:44 +0100

    Tweak webview flags for potential performance improvements

--- src/dash.cpp
+++ src/dash.cpp
@@ -19,16 +19,23 @@ int main(int argc, char *argv[])
             "--no-sandbox "
             "--disable-gpu-sandbox "
             "--disable-features=VaapiVideoDecoder,WebRtcHWDecoding "
-            "--disable-software-rasterizer "
+            "--enable-gpu-rasterization "
+            "--enable-zero-copy "
             "--ignore-gpu-blocklist "
-            "--use-gl=egl");
+            "--use-gl=egl"
+            "--enable-features=VaapiVideoEncoder "
+            "--num-raster-threads=4 "        // Pi 4 has 4 cores, use them
+            "--enable-main-frame-before-activation "
+            "--renderer-process-limit=2 "    // limit renderer processes, saves RAM
+            "--disable-dev-shm-usage "       // Pi /dev/shm is small, avoid it
+            "--memory-pressure-off");        // stop Chromium throttling itself
     qputenv("QT_QPA_EGLFS_HIDECURSOR", "1");
 
     qputenv("QT_QUICK_CONTROLS_STYLE", "Material");
     QApplication dash(argc, argv);
 
     QWebEngineProfile::defaultProfile()->settings()->setAttribute(
-        QWebEngineSettings::Accelerated2dCanvasEnabled, false);
+        QWebEngineSettings::Accelerated2dCanvasEnabled, true);
     QWebEngineProfile::defaultProfile()->settings()->setAttribute(
         QWebEngineSettings::WebGLEnabled, true);
         
